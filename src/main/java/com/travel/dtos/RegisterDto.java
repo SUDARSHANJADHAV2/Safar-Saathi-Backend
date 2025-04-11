94512810060c0335fbeae347ed43043f090d4f54
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-04-11 12:17:35 +0530

    Create RegisterDto.java

---
+++ src/main/java/com/travel/dtos/RegisterDto.java
@@ -0,0 +1,13 @@
+package com.travel.dtos;
+
+import lombok.Data;
+
+@Data
+public class RegisterDto {
+
+    private String name;
+    private String email;
+    private String password;
+
+    private String userRole;
+}
