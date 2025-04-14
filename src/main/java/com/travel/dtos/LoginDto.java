7488995bddecc6466fef71264749b5b08c9ab646
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2025-04-14 12:20:29 +0530

    Create LoginDto.java

---
+++ src/main/java/com/travel/dtos/LoginDto.java
@@ -0,0 +1,13 @@
+package com.travel.dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDto {
+    private String email;
+    private String password;
+}
